018ca761b873418bc35158c9e86df75be8786f11
Raphael Neuberger <info@raphael-neuberger.com>
2025-09-29 14:40:28 +0200

    Add initial book data and implement basic styling
    - Created a new script file for book data with an array of book objects including details like name, author, likes, price, and comments.
    - Added a JavaScript file for rendering books in the container.
    - Established a CSS file for overall styling, including responsive design for various screen sizes.
    - Set up basic structure for header and footer in the CSS.
    - Created empty template and asset CSS files for future use.

---
+++ script.js
@@ -0,0 +1,11 @@
+//Books render in Container
+console.log(books);
+
+// function renderBooks() {
+//   let books_container = document.getElementById("books_container");
+//   for (let renderIndex = 0; renderIndex < books.length; renderIndex++) {
+//     books_container.innerHTML += `<img src="./img/${books[renderIndex]}" alt="${altText[renderIndex]}" >`;
+//   }
+
+//   console.log();
+// }
