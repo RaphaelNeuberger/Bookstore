bd05824b006c33e9860fb1f95ea4dd79d660195e
Raphael Neuberger <info@raphael-neuberger.com>
2025-09-29 17:51:20 +0200

    Aktualisiere Impressum und Index mit korrekten Titel- und Buchdaten; entferne nicht benötigte Bücher-Datenbank

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //Books render in Container
 console.log(books);
 
-// function renderBooks() {
-//   let books_container = document.getElementById("books_container");
-//   for (let renderIndex = 0; renderIndex < books.length; renderIndex++) {
-//     books_container.innerHTML += `<img src="./img/${books[renderIndex]}" alt="${altText[renderIndex]}" >`;
-//   }
+function renderBooks() {
+  let books_container = document.getElementById("books_container");
+  for (let renderIndex = 0; renderIndex < books.length; renderIndex++) {
+    books_container.innerHTML += `<p>${books[renderIndex].name}</p>`;
+  }
 
-//   console.log();
-// }
+  console.log(books);
+}
